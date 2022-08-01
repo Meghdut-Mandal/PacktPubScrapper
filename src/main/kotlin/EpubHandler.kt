0b88ee04977286ccf35e61a7c8b61a4dc26b867e
Meghdut Mandal <meghdut.mandal@autodesk.com>
2022-08-01 16:04:40 +0530

    fixed doctype issue

--- src/main/kotlin/EpubHandler.kt
+++ src/main/kotlin/EpubHandler.kt
@@ -102,7 +102,8 @@ class EpubHandler(
 
 
     private fun generateContent(title: String, data: String) = """
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
 <head>
 <meta charset="utf-8" />
